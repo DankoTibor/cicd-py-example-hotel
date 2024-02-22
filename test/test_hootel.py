0b59bf8cf9a165a6f55dcaa98f531b3f0a3f2574
DankoTibor <146742872+DankoTibor@users.noreply.github.com>
2024-02-22 19:59:41 +0100

    Update test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -27,6 +27,7 @@ class TestHootel(object):
     @allure.tag("login")
     def test_login(self):
         menu_btn = WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable((By.XPATH, '//span[@class="navbar-toggler-icon"]')))
+        menu_btn.click()
         login_btn = WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
         login_btn.click()
 
